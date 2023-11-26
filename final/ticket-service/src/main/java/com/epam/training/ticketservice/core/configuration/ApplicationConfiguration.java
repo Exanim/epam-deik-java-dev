522a762163b121acc2bd1b74fab22f545ce3894d
Exanim <simonbarath7@gmail.com>
2023-11-27 00:12:04 +0100

    Add H2 support

--- final/ticket-service/src/main/java/com/epam/training/ticketservice/core/configuration/ApplicationConfiguration.java
+++ final/ticket-service/src/main/java/com/epam/training/ticketservice/core/configuration/ApplicationConfiguration.java
@@ -5,18 +5,5 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ApplicationConfiguration {
-    @Bean
-    public Bank bank() {
-        return new StaticBank();
-    }
 
-    @Bean
-    public Cart cart(Bank bank) {
-        return Cart.createEmptyCart(bank);
-    }
-
-    @Bean
-    public GrossPriceCalculator calculator() {
-        return new HungarianTaxGrossPriceDecorator(new GrossPriceCalculatorImpl());
-    }
 }
